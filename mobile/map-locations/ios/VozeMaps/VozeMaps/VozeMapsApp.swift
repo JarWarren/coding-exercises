e17cf86796e541dde54a7cab964005eecc79ecf3
Jared Warren <wrrn24@gmail.com>
2024-09-20 21:27:26 -0600

    initial commit

---
+++ mobile/map-locations/ios/VozeMaps/VozeMaps/VozeMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VozeMapsApp.swift
+//  VozeMaps
+//
+//  Created by Jared Warren on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct VozeMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mobile/map-locations/ios/VozeMaps/VozeMapsTests/VozeMapsTests.swift
@@ -0,0 +1,17 @@
+//
+//  VozeMapsTests.swift
+//  VozeMapsTests
+//
+//  Created by Jared Warren on 9/20/24.
+//
+
+import Testing
+@testable import VozeMaps
+
+struct VozeMapsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
